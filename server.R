540b693b12330241a170d292e90e5d7c803e07d4
Carlos R. Mercado <35497842+CR-Mercado@users.noreply.github.com>
2018-01-26 16:28:43 -0500

    WordCloud Maker
    Checked code again locally from fresh environment 
    moved where library(plyr) was loaded for correct over-ride of dplyr count().

--- server.R
+++ server.R
@@ -10,14 +10,13 @@ library(shiny)
 library(dplyr)     
 library(tidytext) #for unnesting
 library(tm)   #for text cleaning
-library(plyr) #for count
 library(RColorBrewer) #for cool colors 
-
+library(wordcloud) #for the wordcloud
 # Define server logic required to create a wordcloud
 shinyServer(function(input, output){
-   
+        
         TheText <- reactive({              # react to changes
-       thebigclean(input$phrase)           # clean the input 
+                thebigclean(input$phrase)           # clean the input 
                 #note: lowercase, no stop words, no foreign letters,
                 # punctuation, numbers, etc. 
         })
@@ -25,47 +24,45 @@ shinyServer(function(input, output){
         TheText.df <- reactive({
                 TheTEXT1 <- as.data.frame(TheText()) #reactive change to df
                 colnames(TheTEXT1) <- "Text"  #change column name
-                   
+                
                 TheTEXT1 #assign this df to the reactive TheText.df
         })
         
-                
+        
         TheNgrams <- reactive({           # react to changes 
                 input$DesiredNgrams      #get the number of n-grams desired
         })
         
         
         ngramify  <- reactive({
-                                #function counts and tables n-grams from text
+                #function counts and tables n-grams from text
                 unnest_tokens(TheText.df(),  #TheText.df is reactive so use "()" 
                               NGRAM,      #The desired column name 
                               Text,   #The current column name
                               token="ngrams", #how to unnest 
                               n=TheNgrams())            #how many words in each group
         })
-        
-  output$TheWordCloud <- renderPlot({
-          with(              #using the reactive datatable 
-                                        #made from unnest_token 
-               count(                   #makes a data table with "n" 
-                                         #counts of each element                       
-                       ungroup(ngramify()),  #ungroup the data.table
-                               NGRAM,        #count this column 
-                               sort = TRUE), #sort it by frequency 
-                wordcloud(NGRAM,             #the column
-                          n,                #the frequencies - front count() 
+        library(plyr)  #need to override dplyr's count() 
+        output$TheWordCloud <- renderPlot({
+                with(              #using the reactive datatable 
+                        #made from unnest_token 
+                        count(                   #makes a data table with "n" 
+                                #counts of each element                       
+                                ngramify(),  #ungroup the data.table
+                                "NGRAM"),        #count this column 
+                        wordcloud(NGRAM,             #the column
+                          freq = freq,   #the frequencies - from count() 
                           max.words = 50,    #most ngrams permitted on wordcloud
                           scale = c(2,.2),   #range of permitted sizes
-                          min.freq = 1,
-                          random.order = FALSE,
-                          random.color = FALSE, #select color by frequency 
+                          min.freq = 1,      #min # of occurances
+                          random.order = FALSE,  #make most common in the middle                                  random.color = FALSE, #select color by frequency 
                           colors = brewer.pal(4,"Dark2") #from RColorBrewer
-                          )       
-               )
-                       
-               })
-  })
-  
+                        )       
+                )
+                
+        })
+})
+
 
 
 
@@ -139,5 +136,3 @@ thebigclean <- function(inputtext, removestops = TRUE,lowercase = TRUE,
 test.words <- c("chicken","burger","cheese","sandwich","the","a","eat","I","want")
 test.sample <- sample(test.words, 500, replace = TRUE)
 test.to.clean <- paste(test.sample, collapse = " ")
-
-
